b95a3802684bb76ce9968b7da5985593d98d4c42
Martin Belda <alu0100832211@ull.edu.es>
2019-07-20 19:21:49 +0100

    Forces to use python 3.7

--- badge.py
+++ badge.py
@@ -9,17 +9,21 @@ def cli():
 
 
 @cli.command()
-@click.argument('json_file', type=click.File('rb'))
-@click.argument('image_file', type=click.File('rb'))
+@click.option("--json", nargs=1, type=click.File('rb'))
+@click.option("--image", nargs=1, type=click.File('rb'))
+#@click.argument('json_file', type=click.File('rb'))
+#@click.argument('image_file', type=click.File('rb'))
 def new(json_file, image_file):
-    openbadge_json = json.load(json_file)
-    png = image_file.read()
-    try:
-        #openbadge_json['image'] = base64.b64encode(png)
-        openbadge_json['image_url'] = "pepe"
-        response=requests.post('http://vituin-chat.iaas.ull.es/api/badges/create', json=openbadge_json)
-        click.echo(response.text)
-        print(openbadge_json)
-    except Exception as error:
-        print(str(error))
-
+    print(json_file)
+    print(image_file)
+#    openbadge_json = json.load(json_file)
+#    png = image_file.read()
+#    try:
+#        #openbadge_json['image'] = base64.b64encode(png)
+#        openbadge_json['image_url'] = "pepe"
+#        response=requests.post('http://vituin-chat.iaas.ull.es/api/badges/create', json=openbadge_json)
+#        click.echo(response.text)
+#        print(openbadge_json)
+#    except Exception as error:
+#        print(str(error))
+#
--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+import sys
+
+if sys.version_info < (3,7):
+    sys.exit("Python 3.7 required!!!")
 
 setup(
     name='badge',
