49c7aef5d6f7670a2530e71128f6d7b09d902b83
Martin Belda <alu0100832211@ull.edu.es>
2019-07-20 21:03:38 +0100

    Creates badge sending image as base64 string

--- badge.py
+++ badge.py
@@ -9,21 +9,15 @@ def cli():
 
 
 @cli.command()
-@click.option("--json", nargs=1, type=click.File('rb'))
-@click.option("--image", nargs=1, type=click.File('rb'))
-#@click.argument('json_file', type=click.File('rb'))
-#@click.argument('image_file', type=click.File('rb'))
-def new(json_file, image_file):
-    print(json_file)
-    print(image_file)
-#    openbadge_json = json.load(json_file)
-#    png = image_file.read()
-#    try:
-#        #openbadge_json['image'] = base64.b64encode(png)
-#        openbadge_json['image_url'] = "pepe"
-#        response=requests.post('http://vituin-chat.iaas.ull.es/api/badges/create', json=openbadge_json)
-#        click.echo(response.text)
-#        print(openbadge_json)
-#    except Exception as error:
-#        print(str(error))
-#
+@click.argument('json_file', type=click.File('rb'))
+@click.argument('image_file', type=click.File('rb'))
+def create(json_file, image_file):
+    openbadge_json = json.load(json_file)
+    png = image_file.read()
+    try:
+        openbadge_json['image_data'] = base64.encodebytes(png).decode('utf-8')
+        response=requests.post('http://vituin-chat.iaas.ull.es/api/badges/create', json=openbadge_json)
+        click.echo(response.text)
+    except Exception as error:
+        print(str(error))
+
