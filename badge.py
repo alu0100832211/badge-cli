cda13a6988084365478f618d90aab56861a19496
Martin Belda <alu0100832211@ull.edu.es>
2019-07-30 17:43:18 +0100

    Update api required field

--- badge.py
+++ badge.py
@@ -15,7 +15,7 @@ def create(json_file, image_file):
     openbadge_json = json.load(json_file)
     png = image_file.read()
     try:
-        openbadge_json['image_data'] = base64.encodebytes(png).decode('utf-8')
+        openbadge_json['image'] = base64.encodebytes(png).decode('utf-8')
         response=requests.post('http://vituin-chat.iaas.ull.es/api/badges/create', json=openbadge_json)
         click.echo(response.text)
     except Exception as error:
