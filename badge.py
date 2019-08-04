b9468053a6a31d22f75ea0d7994dd72c3d961589
Martin Belda <alu0100832211@ull.edu.es>
2019-08-04 12:48:18 +0100

    add uri encoding

--- badge.py
+++ badge.py
@@ -2,6 +2,7 @@ import click
 import requests
 import json
 import base64
+from PIL import Image
 
 @click.group()
 def cli():
@@ -15,7 +16,11 @@ def create(json_file, image_file):
     openbadge_json = json.load(json_file)
     png = image_file.read()
     try:
-        openbadge_json['image'] = base64.encodebytes(png).decode('utf-8')
+        #https://tools.ietf.org/html/rfc2397
+        prefix = 'data:image/{};base64,'.format(str(Image.open(image_file).format).lower())
+        openbadge_json['image'] = prefix + base64.encodebytes(png).decode('utf-8')
+        print(openbadge_json['image'])
+        #openbadge_json['image'] = 'http://pepeppepepe.png'
         response=requests.post('http://vituin-chat.iaas.ull.es/api/badges/create', json=openbadge_json)
         click.echo(response.text)
     except Exception as error:
--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     install_requires=[
         'Click',
         'requests',
+        'pillow'
     ],
     entry_points='''
         [console_scripts]
