efd4add30c9eafbed21cad2ab83401f99ebf6fc9
Martin Belda <alu0100832211@ull.edu.es>
2019-08-04 20:55:27 +0100

    soporte para urls

--- badge.py
+++ badge.py
@@ -11,16 +11,15 @@ def cli():
 
 @cli.command()
 @click.argument('json_file', type=click.File('rb'))
-@click.argument('image_file', type=click.File('rb'))
+@click.argument('image_file', type=click.File('rb'), required=False)
 def create(json_file, image_file):
     openbadge_json = json.load(json_file)
-    png = image_file.read()
     try:
         #https://tools.ietf.org/html/rfc2397
-        prefix = 'data:image/{};base64,'.format(str(Image.open(image_file).format).lower())
-        openbadge_json['image'] = prefix + base64.encodebytes(png).decode('utf-8')
-        print(openbadge_json['image'])
-        #openbadge_json['image'] = 'http://pepeppepepe.png'
+        if image_file:
+            png = image_file.read()
+            prefix = 'data:image/{};base64,'.format(str(Image.open(image_file).format).lower())
+            openbadge_json['image'] = prefix + base64.encodebytes(png).decode('utf-8')
         response=requests.post('http://vituin-chat.iaas.ull.es/api/badges/create', json=openbadge_json)
         click.echo(response.text)
     except Exception as error:
